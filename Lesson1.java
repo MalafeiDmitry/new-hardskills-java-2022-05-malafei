992ad2a7d1185351299952e8184f8049462068fb
Dmitry Malafei <MalafeiDmitry@gmail.com>
2022-08-14 20:17:10 +0300

    Домашнее задание к уроку 1

---
+++ Lesson1.java
@@ -0,0 +1,47 @@
+package HomeWork;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        /*
+ Написать программу на Javа, которая выводит на экран пользователя следующую строку:
+
+H
+e
+l
+l
+o
+
+W
+o
+r
+l
+d
+!
+*/
+
+        System.out.println("H");
+        System.out.println("e");
+        System.out.println("l");
+        System.out.println("l");
+        System.out.println("o");
+        System.out.println(" ");
+        System.out.println("W");
+        System.out.println("o");
+        System.out.println("r");
+        System.out.println("l");
+        System.out.println("d");
+        System.out.println("!");
+
+        System.out.println("_______________________________");
+
+        /*
+ Написать программу на Javа, которая выводит на экран пользователя все числа от 0 до 9 в формате строки (с пробелами):
+
+0 1 2 3 4 5 6 7 8 9
+*/
+        for (int i = 0; i<10;i++){
+            System.out.print(i+" ");
+        }
+
+    }
+}
